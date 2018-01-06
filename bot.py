cad00cb87dde4167177c7f2793725121cd4f0f13
Chris <32988106+Noobot9k@users.noreply.github.com>
2018-01-06 04:42:42 -0700

    Added more readability, made the bot better recognize incorrect use of !roll

--- bot.py
+++ bot.py
@@ -6,9 +6,11 @@ import random
 Client = discord.Client()
 bot_prefix= "!"
 client = commands.Bot(command_prefix=bot_prefix)
+Token = "Mzk4NjQ0MzY1NTU3NDk3ODU2.DTBjlw.iemPtBTrA1xJiiWiAC72ZqPiV5E"
 
 CritLines = ["Headshot!", "Critical Hit!", "Booyeah!", "Crit!", "Finish him!", "Get pwn'd!"]
 CritFailLines = ["Oof", "Fatality!", "Ouch, ooch, oof your bones!", "That'll hurt in the morning..."]
+Roll1D1Lines = ["...What did you think would happen?", "...Why?", "Are you ok?",  "Do you need a doctor?", "What else did you think it would be?"]
 
 @client.event
 async def on_ready():
@@ -37,7 +39,8 @@ async def stats(ctx):
 
 @client.command()
 async def roll(QuantitySides: str):
-	#Identify Devider, Quantity, Sides, Etc...
+	
+	#Identify Devider
 	Devider = 0
 	i = 0
 	while i < len(QuantitySides):
@@ -45,20 +48,36 @@ async def roll(QuantitySides: str):
 		if letter == "D" or letter == "d":
 			Devider = i
 		i = i + 1
-	Quantity = int(QuantitySides[:Devider])
-	Sides    = int(QuantitySides[Devider+1:])
 
 	#Check formatting and tell you to screw off
+	Invalid = False
 	if Devider == 0:
-		await client.say("'{}'? That's not how you roll a dice, silly! Use '!roll xDy' \n x = Number of dice, \n y = Dice sides. \n IE: '!roll 2D6'.".format(QuantitySides))
+		Invalid = True
+	Quantity = 0
+	Sides = 0
+
+	try: #"try:" tries to do the given action and if there's a non-lethal error run the code in "except ValueError" instead of crying in the output. 
+		Quantity = int(QuantitySides[:Devider])		#Identify Quantity
+	except ValueError:
+		Invalid = True
+
+	try: #so right now it's testing if Quantity and Sides are actually numbers and not people trying to hurt senpai.
+		Sides    = int(QuantitySides[Devider+1:])	#Identify Sides
+	except ValueError:
+		Invalid = True
+
+	if Invalid == True:
+		await client.say("'{}'? That's not how you roll a dice, silly! Use '!roll xDy'. \n x = Number of dice, \n y = Dice sides. \n ie: '!roll 2D6'.".format(QuantitySides))
 		return
-	print("Rolling " + str(Quantity)+ " " +str(Sides) + " sided dice.")
+
+	#Check dice quantity
 	if Quantity > 100:
-		print("WHAAA! I can't roll that many dice, you do it!")
+		print("Too many dice detected. WTF?")
 		await client.say("WHAAA! I can't roll that many dice, you do it!")
 		return
 
 	#Actually roll the dice
+	print("Rolling " + str(Quantity)+ " " +str(Sides) + " sided dice.")
 	Total = 0
 	TotalCrits = 0
 	i = 0
@@ -75,23 +94,23 @@ async def roll(QuantitySides: str):
 	#Say the result
 	print("Total is " + str(Total) + " with " + str(TotalCrits) + " crits!")
 	await client.say(str(Total) + " with " + str(TotalCrits) + " crits!")
-	if Quantity == 1 and Total == Sides:
+	if Quantity == 1 and Total == Sides and not Sides == 1:
 		await client.say(CritLines[random.randint(0, len(CritLines)-1 )])
-	if Quantity == 1 and Total == 1:
+	if Quantity == 1 and Total == 1 and not Sides == 1:
 		await client.say(CritFailLines[random.randint(0, len(CritFailLines)-1 )])
 	if Total == 69:
 		await client.say("Nice.")
+	if Quantity == 1 and Sides == 1:
+		await client.say(Roll1D1Lines[random.randint(0, len(Roll1D1Lines)-1 )])
     
 @client.command(pass_context=True)
 async def headpat(ctx):
-    await client.say("Bye!")
-    raise SystemExit
+	await client.say("Bye!")
+	client.logout()
+	raise SystemExit
 
 @client.command()
 async def push(remote: str, branch: str):
     await client.say('Pushing to {} {}'.format(branch, remote))
 
-client.run("Mzk4NjQ0MzY1NTU3NDk3ODU2.DTBjlw.iemPtBTrA1xJiiWiAC72ZqPiV5E")
-
-
-
+client.run(Token)
