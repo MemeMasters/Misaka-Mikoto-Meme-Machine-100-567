ea03a8b093c63a33ace88b5a32d5ca743c61d31c
Benjamin Jacobs <benjammin1100@gmail.com>
2018-09-10 22:35:47 -0600

    Split up commands into seperate classes

--- dm_assist/__init__.py
+++ dm_assist/__init__.py
@@ -1,37 +1,90 @@
 ﻿import os
+from contextlib import contextmanager
+import signal
+
 import discord
 from discord.ext import commands
 
-from dm_assist import config
-from dm_assist.config import config as conf
+from dm_assist.config import config
+
+from dm_assist import voice
+from dm_assist import misc
+from dm_assist import roleplay
 
 
 Bot = commands.Bot(
-	command_prefix=commands.when_mentioned_or(conf['config']['prefix']),
-	description='An attempt to understand the confusing world around me.'
+    command_prefix=commands.when_mentioned_or(config['config']['prefix']),
+    description='An attempt to understand the confusing world around me.'
 )
 
-from dm_assist import voice
+
 Bot.add_cog(voice.Music(Bot))
+Bot.add_cog(roleplay.RolePlay(Bot))
+Bot.add_cog(misc.Misc(Bot))
 
 
-# Prevent circular imports
-from dm_assist import bot
+if not discord.opus.is_loaded():
+    # the 'opus' library here is opus.dll on windows
+    # or libopus.so on linux in the current directory
+    # you should replace this with the location the
+    # opus library is located in and with the proper filename.
+    # note that on windows this DLL is automatically provided for you
+    try:
+        discord.opus.load_opus(config['config']['voice']['opus'])
+    except discord.opus.OpusError as e:
+        print(e)
+        print("Unable to load 'opus' library")
+
+        print("voice cannot be used until opus is configured.")
+        print("Set the path to libopus.so in 'config / voice / opus' in config.yaml")
+
+
+@Bot.event
+async def on_ready():
+    print("Hello Nerds")
+    print("Name: {}".format(Bot.user.name))
+    print("ID: {}".format(Bot.user.id))
+    print("___________________________")
+    #MainChannel = client.get_channel(MainChannelID)
+    #await client.send_message(MainChannel, StartupLines[random.randint(0, len(StartupLines)-1 )])
+
 
 def create_token():
-	if config.config['config'].get('token') is None:
+    if config['config'].get('token') is None:
 
-		print("No token exists!")
-		
-		token = input("Enter Token: ")
-		config.config['config']['token'] = token
+        print("No token exists!")
+        
+        token = input("Enter Token: ")
+        config['config']['token'] = token
 
-		config.save()
+        config.save()
 
+
+@contextmanager
+def sigint_shutdown():
+
+    original_sigint_handler = signal.getsignal(signal.SIGINT)
+
+    def on_shutdown_req():
+        print("Shutting down Bot..")
+        Bot.logout()
+        Bot.close()
+        raise SystemExit
+
+    signal.signal(signal.SIGINT, lambda sig, frame: on_shutdown_req())
+
+    try:
+        print("Press Ctrl-C to stop the Bot")
+        yield
+    except Exception:
+        raise
+    finally:
+        signal.signal(signal.SIGINT, original_sigint_handler)
 
 def serve():
-	create_token()
+    create_token()
 
-	print("Starting Bot..")
-
-	bot.start(config.config['config']['token'])
+    print("Starting Bot..")
+    
+    with sigint_shutdown():
+        Bot.run(config['config']['token'])
---
+++ dm_assist/misc.py
@@ -0,0 +1,97 @@
+import random
+
+from discord.ext import commands
+
+from dm_assist.config import config
+
+dumbLines = config['lines']['dumb']
+StartupLines = config['lines']['startup']
+ShutdownLines = config['lines']['shutdown']
+
+class Misc:
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context = True)
+    async def memberlist(self, ctx):
+        '''Displays members of a server'''
+        memberNames = []
+        memberID = []
+        membernames1 = []
+        for member in ctx.message.server.members:
+            members[member.id] = member
+            memberNames.append(member.name)
+            memberID.append(member.id)
+            derp = []
+            derp.append(member.name)
+            derp.append(member.id)
+            x = ": ".join(map(str, derp)) + "\n"
+            membernames1.append(x)
+        y = "".join(map(str, membernames1))
+        await self.bot.say("```\n" + y + "```")
+
+    @commands.command(pass_context=True)
+    async def bored(self, ctx):
+        '''For people who are bored.'''
+        if ctx.message.author.id == "287697603607658496":
+            await self.bot.say("Play Magic with someone!")
+        else:
+            await self.bot.say("Get over it.")
+            print(ctx.message.author.id + " broke a rule or something")
+
+    @commands.command(pass_context=True)
+    async def weeb(self, ctx):
+        '''Used to call me a weeb. Use with caution.
+        There may or may not be a few lines of code allowing me to 'keep track' of you, supposing you choose to do so.'''
+        await self.bot.say("He's not a weeb!")
+
+
+    #@commands.command()
+    #async def TellMeAMeme(self, ):
+    #	'''Spits out something unfunny.'''
+    #	await self.bot.say(MemeLines[random.randint(0, len(MemeLines)-1 )])
+
+    #@commands.command(pass_context=True)
+    #async def spam(self, ctx, name):
+    #	'''Use this to hurt your friends. Usage: !spam [ID of target]'''
+    #	if name == "287697603607658496":
+    #		await self.bot.say("Nope.")
+    #	else:
+    #		await self.bot.say("Now spamming " + name)
+            #insert all the spamming code here.
+
+    #@commands.command()
+    #async def bug_cats(self, id):
+    #	'''Bug someone.'''
+    #	everything = request.urlopen("https://cat-fact.herokuapp.com/facts").read().decode()
+    #	jsEverything = json.loads(everything)
+    #	catFacts = []
+    #	for item in jsEverything["all"]:
+    #		catFacts.append(item["text"])
+    #	await self.bot.send_message(members[str(id)], "Thanks for subscribing to cat facts!")
+    #	while True:
+    #		await self.bot.wait_until_ready()
+    #		await self.bot.send_message(members[str(id)], catFacts[random.randint(0, len(catFacts) - 1)])
+    #		time.sleep(7)
+
+    @commands.command(pass_context=True)
+    async def headpat(self, ctx, derp: str=1):
+        '''Usage: don't.'''
+        if ctx.message.author.id == "287697603607658496":
+            if derp == 1:
+                await self.bot.say(ShutdownLines[random.randint(0, len(ShutdownLines)-1 )])
+                self.bot.logout()
+                self.bot.close()
+                raise SystemExit
+            else:
+                await self.bot.say(dumbLines[random.randint(0, len(dumbLines) - 1)])
+        else:
+            await self.bot.say(dumbLines[random.randint(0, len(dumbLines) - 1)])
+
+
+    @commands.command(pass_context=True)
+    async def ping(self, ctx):
+        '''Pings the self.bot and prints your id in the output.'''
+        print (ctx.message.author.id + " pinged")
+        await self.bot.say("PONGU!")
