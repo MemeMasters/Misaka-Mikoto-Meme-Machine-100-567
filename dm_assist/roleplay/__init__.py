9e74a3bee636550788c50f3a66fb64463098249b
Benjamin Jacobs <benjammin1100@gmail.com>
2018-12-17 23:27:56 -0700

    Feature: Dice (#17)
    * add equation processing (#16)
    Probably breaks the bot
    
    * Implement quick authentic random numbers
    
    * don't check the randomness of 1 sided dice
    
    * Add ability to print all rolled dice for a command
    
    * Fix equation bugs.
    
    * fix equation syntax error

--- dm_assist/roleplay/__init__.py
+++ dm_assist/roleplay/__init__.py
@@ -1,3 +1,5 @@
+import asyncio
+import math
 from discord.ext import commands
 
 from dm_assist.config import config
@@ -8,55 +10,213 @@ class Roleplay:
 
     def __init__(self, bot):
         self.bot = bot
+    
+    async def print_dice(self, dice):
+        # This is my job security
+        dice_string = ',\n'.join(
+            [', '.join(
+                ["{}/{}".format(x[0], x[1])
+                    for x in dice[i*10:i*10+9]
+                ]) 
+                for i in range(min(int(math.ceil(len(dice) / 10.0)), 4))
+            ])
+
+        await self.bot.say("Rolled:\n```\n{}{}\n```".format(dice_string, '...' if len(dice) > 30 else ''))
 
     @commands.command()
-    async def roll(self, dice: str):
-        '''Rolls X dice with Y sides. Usage: !roll XdY XdY ..'''
+    async def calc(self, *, equation: str):
+        """
+        Calculates an equation.
+
+        You can use other commands such as <times>d<sides>, adv(sides, times),
+        dis(sides, times), top(sides, times, top_num_dice), bot(sides, times, bot_num_dice)
+        """
+        try:
+            util.dice.logging_enabled = True
+            value = util.calculator.parse_equation(equation)
+            util.dice.logging_enabled = False
+        except util.BadEquation as exception:
+            await self.bot.say("{} Tell me again, but slower..".format(exception))
+            return
+        
+        dice = util.dice.rolled_dice
+        if len(dice) > 0:
+            await self.print_dice(dice)
+
+        await self.bot.say("According to my notes, the answer is: **{}**".format(value))
+
+        if util.dice.low:
+            asyncio.ensure_future(util.dice.load_random_buffer())
+
+
+    @commands.command()
+    async def roll(self, roll: str):
+        '''Rolls X dice with Y sides. Usage: !roll XdY + 5..'''
+
 
         try:
-            data = util.parse_die_roll(dice)
-        except util.BadFormat as exception:
-            await self.bot.say(exception)
+            roll = roll.lower().split("d")
+
+            util.dice.logging_enabled = True
+            data = util.dice.roll_sum(int(roll[1]), int(roll[0]))
+            util.dice.logging_enabled = False
+        except IndexError:
+            await self.bot.say("I can't understand what you're trying to say, the format is `<times>d<sides>`")
+            return
+        except ValueError:
+            await self.bot.say("You're suppost to use numbers!")
             return
 
-        total = data['total']
+        dice = util.dice.rolled_dice
+        if len(dice) > 1:
+            await self.print_dice(dice)
+
+        total = data[0]
+        rolls = roll[0]
+        sides = roll[1]
 
         # + " with " + str(TotalCrits) + " crits!")
-        if data['rolls'] is 1:
+        if rolls is 1:
             await self.bot.say(str(total))
             
-            if data['sides'][0] is 1:
+            if sides is 1:
                 await self.bot.say(util.get_random_line(config.lines.dumb))
-            elif data['crits'] is 1:
+            elif data[1] is 1:
                 await self.bot.say(util.get_random_line(config.lines.crits))
-            elif data['fails'] is 1:
+            elif data[2] is 1:
                 await self.bot.say(util.get_random_line(config.lines.critFails))
         else:
             message = str(total)
             
-            if data['sides'][0] is 1:
+            if sides is 1:
                 await self.bot.say(message)
                 await self.bot.say(util.get_random_line(config.lines.dumb))
             else:
-                if data['crits'] > 0:
-                    message += " with {} crits".format(data['crits'])
+                if data[1] > 0:
+                    message += " with {} crits".format(data[1])
 
-                if data['fails'] > 0:
+                if data[2] > 0:
                     message += "{} {} fails".format(
-                        " with" if data['crits'] is 0 else ", and",
-                        data['fails'])
+                        " with" if data[1] is 0 else ", and",
+                        data[2])
                 await self.bot.say(message)
 
         for key, val in config['lines']['on_roll'].items():
             if str(total) == key:
                 await self.bot.say(util.get_random_line(val))
                 break
+        
+        if util.dice.low:
+            asyncio.ensure_future(util.dice.load_random_buffer())
 
-    @commands.command(pass_context=True) #coinflip stuff
-    async def coinflip(self, ctx):
+    @commands.command() #coinflip stuff
+    async def coinflip(self):
         '''Flips a coin.'''
-        HeadTails, _, _ = util.roll(1, 2)
+        HeadTails = util.dice.roll(2)
+
         if HeadTails == 1:
             await  self.bot.say("Tails, but you're dead either way")
         else:
             await  self.bot.say("Heads, but you're dead either way")
+
+        if util.dice.low:
+            asyncio.ensure_future(util.dice.load_random_buffer())
+        
+    
+    @commands.command()
+    async def adv(self, sides='20'):
+        """
+        Rolls a die with advantage. Usage: adv [sides]
+        """
+        try:
+            sides = int(sides)
+        except ValueError:
+            self.bot.say("That's not a number, silly.")
+            return
+
+        d1 = util.dice.roll(sides)
+        d2 = util.dice.roll(sides)
+
+        final = max(d1, d2)
+
+        await self.bot.say("You rolled a {}, and a {}.\n you got a **{}**.".format(d1, d2, final))
+        if d1 is d2:
+            await self.bot.say("You're dead either way :)")
+    
+        if util.dice.low:
+            asyncio.ensure_future(util.dice.load_random_buffer())
+
+    @commands.command()
+    async def dis(self, sides='20'):
+        """
+        Rolls a die with disadvantage. Usage: dis [sides=20]
+        """
+        try:
+            sides = int(sides)
+        except ValueError:
+            self.bot.say("That's not a number, silly.")
+            return
+
+        d1 = util.dice.roll(sides)
+        d2 = util.dice.roll(sides)
+
+        final = min(d1, d2)
+
+        await self.bot.say("You rolled a {}, and a {}.\n you got a **{}**.".format(d1, d2, final))
+        if d1 is d2:
+            await self.bot.say("You're dead either way :)")
+
+        if util.dice.low:
+            asyncio.ensure_future(util.dice.load_random_buffer())
+    
+    @commands.command()
+    async def top(self, times='4', sides='6', top_dice='3'):
+        """
+        Rolls a number of dice, and takes only the top dice.  Usage: top [times=4] [sides=6] [num_top_dice=3]
+        """
+
+        try:
+            sides = int(sides)
+            times = int(times)
+            top_dice = int(top_dice)
+        except ValueError:
+            self.bot.say("You're supposed to enter number not whatever that was")
+
+        util.dice.logging_enabled = True
+        sum = util.dice.roll_top(sides, top_dice, times)
+        util.dice.logging_enabled = False
+
+        dice = util.dice.rolled_dice
+        if len(dice) > 1:
+            await self.print_dice(dice)
+        
+        await self.bot.say("You got **{}**".format(sum))
+        
+        if util.dice.low:
+            asyncio.ensure_future(util.dice.load_random_buffer())
+    
+    @commands.command(name='bot')
+    async def bottom(self, times='4', sides='6', top_dice='3'):
+        """
+        Rolls a number of dice, and takes only the bottom dice.  Usage: bot [times=4] [sides=6] [num_top_dice=3]
+        """
+
+        try:
+            sides = int(sides)
+            times = int(times)
+            top_dice = int(top_dice)
+        except ValueError:
+            self.bot.say("You're supposed to enter number not whatever that was")
+
+        util.dice.logging_enabled = True
+        sum = util.dice.roll_top(sides, top_dice, times, False)
+        util.dice.logging_enabled = False
+
+        dice = util.dice.rolled_dice
+        if len(dice) > 1:
+            await self.print_dice(dice)
+        
+        await self.bot.say("You got **{}**".format(sum))
+        
+        if util.dice.low:
+            asyncio.ensure_future(util.dice.load_random_buffer())
---
+++ dm_assist/util/_calculator.py
@@ -0,0 +1,182 @@
+import re
+
+from . import dice, BadEquation
+
+
+class Calculator:
+    """
+    This parses textual equations, and calculates the result.
+
+    If you want to add your own functions, all you need to do is to set 
+    its precidence in the precidence variable, set the number of operands
+    needed if it has more or less than 2, and create the function as a lambda
+    in the functions dict.
+
+    """
+
+    # Lower numbers mean a lower precidence (it is less important)
+    precidence = {
+        '+': 2, '-': 2,
+        '*': 3, '/': 3,
+        '^':4, '%':4,
+        'd': 5,
+        'round': 6, 'adv': 6, 'dis': 6, 'top': 6, 'bot': 6
+    }
+    
+    # A function by default has 2 arguments, if it does not, list the number required here.
+    function_length = {
+        'round': 1,
+        'adv': 1,
+        'dis': 1,
+        'top': 3,
+        'bot': 3
+    }
+
+    # All the functions are defined here as lambdas.
+    functions = {
+        '+': lambda a, b: a + b,
+        '-': lambda a, b: a - b,
+        '*': lambda a, b: a * b,
+        '/': lambda a, b: a / b,
+        '^': lambda a, b: a ** b,
+        '%': lambda a, b: a % b,
+        'd': lambda a, b: dice.roll_sum(round(b), round(a))[0],
+        'adv': lambda a: dice.roll_top(round(a), 1, 2),
+        'dis': lambda a: dice.roll_top(round(a), 1, 2, False),
+        'top': lambda a, b, c: dice.roll_top(round(b), round(c), round(a)),
+        'bot': lambda a, b, c: dice.roll_top(round(b), round(c), round(a), False),
+        'round': lambda a: round(a)
+    }
+
+    def _load_equation(self, data: list) -> list:
+        """
+        Parse an equation to be calculated easier by a computer using the
+        Shunting Yard Algorithm.
+
+        A SYA equation looks like this:
+
+        ```
+        5 + 4 * 3 => 5 4 3 * +
+        ```
+        """
+        stack = list()
+        num_parens = 0
+
+        equation = list()
+
+        for i in data:
+            try:
+                number = float(i)
+                equation.append(number)
+            except ValueError:  # If the item is not a number, it must be an operator
+                # Check if the item is the end of a paranthesis
+                if i == ')':
+                    # If so, pop all the operands up to the acompanying paranthesis
+                    num_parens -= 1
+                    while len(stack) > 0:
+                        pop = stack.pop()
+                        if pop == '(':
+                            break
+                        if pop != ',':
+                            equation.append(pop)
+                else:
+                    if i == '(':
+                        num_parens += 1
+                    elif i == ',':  # commas are treated like paranthathese
+                        broken = False
+                        while len(stack) > 0:
+                            peek = stack[-1]
+                            if peek != '(':
+                                stack.pop()
+                            if peek == ',' or peek == '(':
+                                broken = True
+                                break
+                            equation.append(peek)
+                        if not broken:
+                            raise BadEquation("Improper use of commas.")
+                    else:
+                        # If the precidence of the stack is greater than the current precidence, than pop until it's not
+                        while len(stack) > 0 and self.__class__.precidence.get(i, 0) <= self.__class__.precidence.get(stack[-1], 0):
+                            pop = stack.pop()
+                            if pop == '(':
+                                raise BadEquation("Mismatched parentheses.")
+                            equation.append(pop)
+                    # Add the operator to the stack
+                    stack.append(i)
+        
+        if num_parens is not 0:
+            raise BadEquation("Mismatched parentheses.")
+        
+        while len(stack) > 0:
+            equation.append(stack.pop())
+        
+        return equation
+    
+    def _calculate_equation(self, equation: list) -> float:
+        """
+        calculate a Shunting Yard equation.
+        """
+        stack = list()
+
+        for i in equation:
+            if isinstance(i, float):
+                stack.append(i)
+            else:
+                try:
+                    try:
+                        # Load the operands
+                        operands = list()
+                        for _ in range(self.__class__.function_length.get(i, 2)):
+                            operands.insert(0, stack.pop())
+
+                        # Process the function
+                        stack.append(self.__class__.functions[i](*operands))
+                    except IndexError:
+                        raise BadEquation("Unbalanced number of operators or operands.")
+                except IndexError:
+                    raise BadEquation("Invalid Operator '{}'.".format(i))
+        
+        if len(stack) is not 1:
+            raise BadEquation("Invalid number of operands.")
+        
+        return stack.pop()
+
+    def parse_equation(self, string: str) -> float:
+        """
+        Parse a human readable equation.
+
+        This supports the following operators:
+
+        ```
+        + - * / ^ % d ( ) round(x)
+        ```
+
+        Note: d is used for rolling dice where the firstoperand is the number of dice
+        to roll, and the second is the number of faces.
+        
+        an example of an equation:
+
+        ```
+        ((5 * 4 + 3 / 6) % 6)d6 = 2d6
+        ```
+
+        If there is a formatting problem with the given equation, a BadEquation error
+        will be thrown.
+        """
+        # parse the string into a list of operators and operands.
+
+        # (?|?|?) regex or
+        # [^\w.,\s] matches any single character that is not a word, number, _, or whitespace
+        # [\d.]+ matches any number
+        # [a-z]+ matches any lower case word
+        # goto https://regex101.com/ for help creating your own regexes.
+        equation = re.findall(r"([^\w.\s]|[\d.]+|[a-z]+)", string.lower())
+
+        # Parse the equation using the Shunting Yard Algorithm
+        equation = self._load_equation(equation)
+
+        # Find the answer to the equation
+        value = self._calculate_equation(equation)
+
+        # Force the result into an int if it's an integer value
+        return int(value) if value == int(value) else value
